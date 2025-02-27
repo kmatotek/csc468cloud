5cff4bbce2b1a6172b760eedda76efe6309d1fef
kmatotek <kadinstitch@gmail.com>
2025-02-27 14:01:29 -0500

    edit

--- profile.py
+++ profile.py
@@ -5,6 +5,8 @@ import geni.rspec.pg as rspec
 request = portal.context.makeRequestRSpec()
 # Create a XenVM
 node = request.XenVM("node")
+node.cores = 4
+node.ram = 4096
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 node.routable_control_ip = "true"
 
