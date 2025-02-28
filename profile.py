9cdd277c5226710451d980b1713159a971bb62e7
kmatotek <kadinstitch@gmail.com>
2025-02-27 20:07:34 -0500

    ok

--- profile.py
+++ profile.py
@@ -9,8 +9,8 @@ request = portal.context.makeRequestRSpec()
 # Allocate a node with the correct hardware configuration.
 node = request.RawPC("node")
 node.hardware_type = "nvidiagh"
-node.cores = 4  # 8-core CPU (Arm Neoverse V2)
-node.ram = 32000  # 100GB RAM (in MB)
+#node.cores = 4  # 8-core CPU (Arm Neoverse V2)
+#node.ram = 32000  # 100GB RAM (in MB)
 
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
 
