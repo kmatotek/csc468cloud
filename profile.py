294e9bcdc1d2f672208aa8adc899d6a65a260c90
kmatotek <kadinstitch@gmail.com>
2025-03-03 13:53:28 -0500

    cool

--- profile.py
+++ profile.py
@@ -8,7 +8,7 @@ request = portal.context.makeRequestRSpec()
 
 # Allocate a node with the correct hardware configuration.
 node = request.RawPC("node")
-node.hardware_type = "rs620"
+node.hardware_type = "r6525"
 # Use r650, r625, m400
 #node.cores = 4  # 8-core CPU (Arm Neoverse V2)
 #node.ram = 32000  # 100GB RAM (in MB)
@@ -19,7 +19,7 @@ node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD
 node.addService(rspec.Execute(shell="bash", command="echo Using GPU"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y git python3 apache2"))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install python3-pip"))
+node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y python3-pip"))
 node.addService(rspec.Execute(shell="/bin/sh", command="pip install requests lxml ollama"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install python3-pip"))
 node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
