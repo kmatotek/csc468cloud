31d7d9fea3b1fcc572864088eb8aba9839b2f788
kmatotek <kadinstitch@gmail.com>
2025-03-17 16:54:03 -0400

    d

--- profile.py
+++ profile.py
@@ -8,7 +8,8 @@ request = portal.context.makeRequestRSpec()
 
 # Allocate a node with the correct hardware configuration.
 node = request.RawPC("node")
-node.hardware_type = "d760-gpu"
+node.hardware_type = "d760p"
+
 # Use r650, r625, m400, d760-gpu, d760p, c6220, c6525-100g
 #node.cores = 4  # 8-core CPU (Arm Neoverse V2)
 #node.ram = 32000  # 100GB RAM (in MB)
